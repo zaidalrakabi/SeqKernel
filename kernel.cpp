429158a4d9ddd27ceb8a2dcd05b917db2665ac14
Zaid Haydar Al Rakabi <zalrakab@pc5.cs.ucdavis.edu>
2018-01-19 12:58:38 -0800

    Starting point

---
+++ kernel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int computeK1();
+void readBL62(double *bl62[]);
+
+int main(int argc, char* argv[]){
+	double* bl62[20];
+	readBL62(bl62);
+	S1 = computeK1(double * bl62[]);
+}
+
+void readBL62(double *bl62[], argv[0]){
+	memset(bl62, 0, 20*sizeof(double));
+	ifstream inf(argv[1);
+
+}	
